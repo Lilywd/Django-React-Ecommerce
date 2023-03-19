0cd6588acb74e72a0e09439882dc3c405843cc2c
lilywd <ndungulilianwanjiku@gmail.com>
2023-03-19 13:31:21 +0300

    backend djoser authentication done

--- Backend/Authentication/authentication/settings.py
+++ Backend/Authentication/authentication/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hu0()y_&&ea3@ly#u3iwh3inf@m@k(zyq$_k)b09q935i@sq^u"
+SECRET_KEY=env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
